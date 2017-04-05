d97bb19718c0e8a27f7126ea8822d566e4735802
Per Manniche Bering <pmbering@gmail.com>
2017-04-05 11:32:04 +0200

    Add project files.

---
+++ src/SitecorePrometheusMetrics.Core/MetricsRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace SitecorePrometheusMetrics.Core
+{
+    public class MetricsRequest : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            var context = args.Context;
+
+            if (!context.Request.Url.PathAndQuery.Equals("/metrics", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            args.AbortPipeline();
+
+            context.Response.Clear();
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("hello");
+
+            context.ApplicationInstance.CompleteRequest();
+        }
+    }
+}
